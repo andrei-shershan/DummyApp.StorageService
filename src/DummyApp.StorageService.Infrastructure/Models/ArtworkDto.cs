26aa276c5f19c04b86cc60c91f39969af99cf8d9
Andrei Shershan <mail.shershan@gmail.com>
2026-08-22 10:41:05 +0200

    Remove Series from Artwork

--- src/DummyApp.StorageService.Infrastructure/Models/ArtworkDto.cs
+++ src/DummyApp.StorageService.Infrastructure/Models/ArtworkDto.cs
@@ -13,5 +13,4 @@ public sealed record ArtworkDto
     public string ImgUrl { get; init; } = string.Empty;
     public string ThumbnailUrl { get; init; } = string.Empty;
     public bool IsActive { get; init; }
-    public string? SeriesName { get; init; }
 }
